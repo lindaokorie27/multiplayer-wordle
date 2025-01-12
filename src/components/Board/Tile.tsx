a4f9d00da40f34fffb8aec334a0c45c720559dd9
Linda Okorie-Kalu <lindaokoriekalu27@gmail.com>
2025-01-12 11:22:49 +0000

    created basic pages with no functionality

---
+++ src/components/Board/Tile.tsx
@@ -0,0 +1,18 @@
+import { TileType as TileProps } from "@/types/status";
+import classNames from "classnames";
+import { FC } from "react";
+
+const Tile: FC<TileProps> = (props) => {
+  const { value, status } = props;
+  const classes = classNames(
+    "h-14 w-14 inline-flex border-2 items-center justify-center font-bold uppercase text-2xl",
+    {
+      "border-slate-300": !status,
+      "border-slate-500 bg-slate-500 text-white": status === "ABSENT",
+      "border-yellow-500 bg-yellow-500 text-white": status === "PRESENT",
+      "border-green-500 bg-green-500 text-white": status === "CORRECT",
+    }
+  );
+  return <div className={classes}>{value}</div>;
+};
+export default Tile;
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,4 @@
+const Navigation = () => {
+  return <div className="border-b border-slate-300 "> Navigation</div>;
+};
+export default Navigation;
